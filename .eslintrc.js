4871231579e31444ca368fb56df7bfd4d3cd9eb7
Alen Yohannan <66544823+AlenSaito1@users.noreply.github.com>
2021-06-15 20:48:00 +0530

    Configure `ESlint`

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,25 @@
-{
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
     "extends": [
-      "react-app",
-      "react-app/jest"
-    ]
-}
+        "eslint:recommended",
+        "plugin:react/recommended",
+        "plugin:@typescript-eslint/recommended"
+    ],
+    "parser": "@typescript-eslint/parser",
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "ecmaVersion": 12,
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react",
+        "@typescript-eslint"
+    ],
+    "rules": {
+    }
+};
